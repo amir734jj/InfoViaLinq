f611e81757929d47486a496b86b07984c698e06f
Amir Hesamian <hesamian@uwm.edu>
2026-06-26 15:53:44 -0500

    Add missing Action FuncLambda overloads to interface; migrate CI to GitHub Actions

--- Core/Interfaces/IGetPropertyNameViaLinq.cs
+++ Core/Interfaces/IGetPropertyNameViaLinq.cs
@@ -10,25 +10,29 @@ namespace InfoViaLinq.Interfaces
     public interface IInfoViaLinq<TSource>
     {
         #region Property
-        
+
         IGetPropInfo<TSource> PropLambda(Expression<Func<TSource, object>> expr);
 
         #endregion
-        
+
         #region Method
 
         IGetFuncInfo<TSource> FuncLambda(Expression<Func<TSource, Action>> expression);
 
         IGetFuncInfo<TSource> FuncLambda<TParam>(Expression<Func<TSource, Action<TParam>>> expression);
 
+        IGetFuncInfo<TSource> FuncLambda<TParam1, TParam2>(Expression<Func<TSource, Action<TParam1, TParam2>>> expression);
+
+        IGetFuncInfo<TSource> FuncLambda<TParam1, TParam2, TParam3>(Expression<Func<TSource, Action<TParam1, TParam2, TParam3>>> expression);
+
         IGetFuncInfo<TSource> FuncLambda<TResult>(Expression<Func<TSource, Func<TResult>>> expression);
-        
+
         IGetFuncInfo<TSource> FuncLambda<TParam, TResult>(Expression<Func<TSource, Func<TParam, TResult>>> expression);
 
         IGetFuncInfo<TSource> FuncLambda<TParam1, TParam2, TResult>(Expression<Func<TSource, Func<TParam1, TParam2, TResult>>> expression);
 
         IGetFuncInfo<TSource> FuncLambda<TParam1, TParam2, TParam3, TResult>(Expression<Func<TSource, Func<TParam1, TParam2, TParam3, TResult>>> expression);
-        
+
         #endregion
     }
 }
